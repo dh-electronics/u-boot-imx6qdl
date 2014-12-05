2d58a757a615e56d335f8c332a10eac5d4e1d524
Ludwig Zenz <lzenz@dh-electronics.de>
2014-12-05 14:29:33 +0100

    LZ: rename of devicetree file, comment out hdmi code

--- include/configs/dh_imx6.h
+++ include/configs/dh_imx6.h
@@ -9,6 +9,8 @@
 #ifndef __DH_IMX6_CONFIG_H
 #define __DH_IMX6_CONFIG_H
 
+//#define DEBUG
+
 #include <asm/arch/imx-regs.h>
 #include <asm/imx-common/gpio.h>
 
@@ -17,9 +19,9 @@
 #define CONFIG_CONSOLE_DEV		"ttymxc0"
 #define CONFIG_MMCROOT			"/dev/mmcblk1p2"
 #if defined(CONFIG_MX6Q)
-#define CONFIG_DEFAULT_FDT_FILE	"imx6q-sabresd.dtb"
+#define CONFIG_DEFAULT_FDT_FILE	"imx6q-dheva01.dtb"
 #elif defined(CONFIG_MX6DL)
-#define CONFIG_DEFAULT_FDT_FILE	"imx6dl-sabresd.dtb"
+#define CONFIG_DEFAULT_FDT_FILE	"imx6dl-dheva01.dtb"
 #endif
 #define PHYS_SDRAM_SIZE		(1u * 1024 * 1024 * 1024)
 
@@ -44,6 +46,6 @@
 #define CONFIG_VIDEO_LOGO
 #define CONFIG_VIDEO_BMP_LOGO
 #define CONFIG_IPUV3_CLK 260000000
-#define CONFIG_IMX_HDMI
+//#define CONFIG_IMX_HDMI
 
 #endif                         /* __DH_IMX6_CONFIG_H */

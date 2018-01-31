a0160ca8d7fe5b0217a0e4d0f8487eaf390d4d8b
Ludwig Zenz <lzenz@dh-electronics.de>
2018-01-31 08:56:27 +0100

    spl: mmc: add support for BOOT_DEVICE_MMC3

--- arch/arm/mach-imx/spl.c
+++ arch/arm/mach-imx/spl.c
@@ -135,6 +135,7 @@ u32 spl_boot_mode(const u32 boot_device)
 	/* for MMC return either RAW or FAT mode */
 	case BOOT_DEVICE_MMC1:
 	case BOOT_DEVICE_MMC2:
+	case BOOT_DEVICE_MMC3:
 #if defined(CONFIG_SPL_FAT_SUPPORT)
 		return MMCSD_MODE_FS;
 #elif defined(CONFIG_SUPPORT_EMMC_BOOT)
--- common/spl/spl_mmc.c
+++ common/spl/spl_mmc.c
@@ -105,6 +105,8 @@ static int spl_mmc_get_device_index(u32 boot_device)
 	case BOOT_DEVICE_MMC2:
 	case BOOT_DEVICE_MMC2_2:
 		return 1;
+	case BOOT_DEVICE_MMC3:
+		return 2;
 	}
 
 #ifdef CONFIG_SPL_LIBCOMMON_SUPPORT
@@ -368,3 +370,4 @@ int spl_mmc_load_image(struct spl_image_info *spl_image,
 SPL_LOAD_IMAGE_METHOD("MMC1", 0, BOOT_DEVICE_MMC1, spl_mmc_load_image);
 SPL_LOAD_IMAGE_METHOD("MMC2", 0, BOOT_DEVICE_MMC2, spl_mmc_load_image);
 SPL_LOAD_IMAGE_METHOD("MMC2_2", 0, BOOT_DEVICE_MMC2_2, spl_mmc_load_image);
+SPL_LOAD_IMAGE_METHOD("MMC3", 0, BOOT_DEVICE_MMC3, spl_mmc_load_image);

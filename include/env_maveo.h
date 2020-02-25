97c8df44f5dbeb1b105ff95e7ef50f2f5c00b5ae
Simon Stürz <simon.stuerz@guh.io>
2020-02-25 16:57:30 +0100

    Add maveo LED commands to u-boot environment

--- include/env_maveo.h
+++ include/env_maveo.h
@@ -41,11 +41,75 @@
     "echo --> Boot arguments: ${bootargs}; "                            \
     "mmc dev ${mmcdev}:1; "                                             \
     "load mmc ${mmcdev}:1 ${loadaddr} zImage_imx6ull-v1_emmc.update; "  \
-    "bootz ${loadaddr};\0"                                              \
+    "run maveo_led_boot_recovery; "                                     \
+    "bootz ${loadaddr}; "                                               \
+    "run maveo_led_error;\0"                                            \
                                                                         \
     "maveo_autoupdate="                                                 \
     "setenv auto_update 0; saveenv; "                                   \
-    "update auto;\0"                                                    \
+    "run maveo_led_boot_flasher; "                                      \
+    "update auto; "                                                     \
+    "run maveo_led_error;\0"                                            \
+                                                                        \
+    "maveo_led_initialize="                                             \
+    "echo --> Init LED controller; "                                    \
+    "echo --> Enable PSoC reset pin; "                                  \
+    "gpio set 130; "                                                    \
+    "i2c dev 1; "                                                       \
+    "if i2c probe 0x08; then"                                           \
+    " echo LED controller found. Perform software reset;"               \
+    " i2c mw 0x08.1 0x01 0xFF;"                                         \
+    "else"                                                              \
+    " echo LED controller could not be found; "                         \
+    "fi;\0"                                                             \
+                                                                        \
+    "maveo_led_boot_system="                                            \
+    "echo --> Set LED into boot mode; "                                 \
+    "i2c dev 1; "                                                       \
+    "if i2c probe 0x08; then"                                           \
+    " i2c mw 0x08.1 0x01 0x00;"                                         \
+    " i2c mw 0x08.1 0x05 0x00;"                                         \
+    " i2c mw 0x08.1 0x06 0x00;"                                         \
+    " i2c mw 0x08.1 0x07 0xFF;"                                         \
+    "else"                                                              \
+    " echo LED controller could not be found; "                         \
+    "fi;\0"                                                             \
+                                                                        \
+    "maveo_led_boot_flasher="                                           \
+    "echo --> Set LED into boot flasher mode; "                         \
+    "i2c dev 1; "                                                       \
+    "if i2c probe 0x08; then"                                           \
+    " i2c mw 0x08.1 0x01 0x0A;"                                         \
+    " i2c mw 0x08.1 0x03 0x01;"                                         \
+    " i2c mw 0x08.1 0x04 0x90;"                                         \
+    "else"                                                              \
+    " echo LED controller could not be found; "                         \
+    "fi;\0"                                                             \
+                                                                        \
+    "maveo_led_boot_recovery="                                          \
+    "echo --> Set LED into boot mode; "                                 \
+    "i2c dev 1; "                                                       \
+    "if i2c probe 0x08; then"                                           \
+    " i2c mw 0x08.1 0x01 0x08;"                                         \
+    " i2c mw 0x08.1 0x03 0x01;"                                         \
+    " i2c mw 0x08.1 0x04 0x90;"                                         \
+    "else"                                                              \
+    " echo LED controller could not be found; "                         \
+    "fi;\0"                                                             \
+                                                                        \
+    "maveo_led_error="                                                  \
+    "echo --> Set LED into error mode; "                                \
+    "i2c dev 1; "                                                       \
+    "if i2c probe 0x08; then"                                           \
+    " i2c mw 0x08.1 0x01 0x02;"                                         \
+    " i2c mw 0x08.1 0x03 0x00;"                                         \
+    " i2c mw 0x08.1 0x04 0xC8;"                                         \
+    " i2c mw 0x08.1 0x05 0xFF;"                                         \
+    " i2c mw 0x08.1 0x06 0x00;"                                         \
+    " i2c mw 0x08.1 0x07 0x00;"                                         \
+    "else"                                                              \
+    " echo LED controller could not be found; "                         \
+    "fi;\0"                                                             \
                                                                         \
     "maveo_resetbuttoncheck="                                           \
     "if gpio input 8; then"                                             \
@@ -76,6 +140,7 @@
                                                                         \
     "maveoboot="                                                        \
     "echo --> Run maveo boot command; "                                 \
+    "run maveo_led_initialize; "                                        \
     "setenv bootenv_file /boot/u-boot.env; "                            \
     "setenv fdt_addr_r ${fdt_addr}; "                                   \
     "setenv kernel_addr_r ${loadaddr}; "                                \
@@ -83,6 +148,8 @@
     "mmc dev ${mmcdev}; "                                               \
     "run maveo_resetbuttoncheck; "                                      \
     "run maveo_autoupdatecheck; "                                       \
-    "run maveo_autorecoverycheck;\0"                                    \
+    "run maveo_autorecoverycheck; "                                     \
+    "run maveo_led_boot_system;\0"                                      \
+
 
 #endif /* HEADER_ENV_MAVEO_H */
